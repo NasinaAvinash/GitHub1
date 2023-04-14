3bb8ba9984d6a70124cac54e120ff2f1497f5697
HP <HP@DESKTOP-UTC920B>
2023-04-14 19:30:28 +0530

    added Additional checks

--- src/project1/Class2.java
+++ src/project1/Class2.java
@@ -3,7 +3,7 @@ package project1;
 public class Class2 {
 
 	public static void main(String[] args) {
-		System.out.println("the test was passed sucessfully");
+		System.out.println("the test was passed sucessfully ======== Ippatike Masthu indhi code chalu inka");
 		
 
 	}
